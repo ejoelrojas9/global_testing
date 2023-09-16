9f4e86864a20f9e6383a9e2ecd62f4e9fc0fde25
ejoelrojas9 <96266652+ejoelrojas9@users.noreply.github.com>
2023-09-15 23:01:49 -0500

    Crud task configured

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,4 +1,49 @@
 class CandidatesController < ApplicationController
+  
   def index
+    @candidates = Candidate.all.order(id: :asc)
   end
+
+  def show
+    set_candidate
+  end
+
+  def create
+    @candidate = Candidate.new candidate_params
+    if @candidate.save
+      flash[:notice] = "Candidate created"
+      redirect_to candidate_path(@candidate)
+    else
+      flash[:alert] = @manager.errors.full_messages.join(',')
+    end
+  end
+
+  def update
+    set_candidate
+    if @candidate.update candidate_params
+      redirect_to candidate_path(@candidate), notice: "Candidate updated"
+    else
+      flash[:alert] = @candidate.errors.full_messages.join(', ')
+    end
+  end
+
+  def destroy
+    set_candidate
+    if @candidate.destroy
+      redirect_to candidates_path, alert: "Candidate deleted"
+    else
+      flash[:alert] = @candidate.errors.full_messages.join(', ')
+    end
+  end
+
+  private
+
+  def set_candidate
+    @candidate = Candidate.find params[:id]
+  end
+
+  def candidate_params
+    params.require(:candidate).permit!  
+  end
+
 end
